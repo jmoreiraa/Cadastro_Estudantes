a6faeaa6651a36befe36c74074fe91302d750a2e
jmoreiraa <jsds95@gmail.com>
2021-12-09 10:09:06 -0300

    modificações Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListAlunosComponent } from './list-alunos/list-alunos.component';
 import { CadAlunoComponent } from './cad-aluno/cad-aluno.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CadAlunoComponent } from './cad-aluno/cad-aluno.component';
     CadAlunoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list-alunos/list-alunos.component.ts
+++ src/app/list-alunos/list-alunos.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { empty, Observable, Subject } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Aluno } from './aluno';
-import { ListAlunosService } from './list-alunos.service';
+
 
 @Component({
   selector: 'list-alunos',
@@ -11,31 +10,10 @@ import { ListAlunosService } from './list-alunos.service';
 })
 export class ListAlunosComponent implements OnInit {
 
-  //alunos: Aluno[] = [];
-
-  alunos$: Observable<Aluno[]> | undefined;
-  error$ = new Subject<boolean>();
-
-  constructor(private service: ListAlunosService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getAlunos();
-  }
-
-  getAlunos() {
-    // this.service.list().subscribe(alunos => this.alunos = alunos);
-    this.onRefresh();
-  }
-
-  onRefresh() {
-    this.alunos$ = this.service.list()
-    .pipe(
-      catchError(error => {
-        console.error(error);
-        this.error$.next(true);
-        return empty();
-      })
-    );
+    
   }
 
 }
